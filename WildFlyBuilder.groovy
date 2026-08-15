10896912ce1aa0e42ee3db3fb223dc88accc396f
Gator-Go <gator@sw-builder.com>
2026-08-15 13:51:03 -0400

    documentation changes

--- WildFlyBuilder.groovy
+++ WildFlyBuilder.groovy
@@ -979,7 +979,7 @@ void createApp(String templatePath, String appDir, def configs) {
 }
 // ====================== EXECUTION ======================
 def configs = loadConfigs()
-def appDir = "./" + configs.appNames.appName[1].'@newName'
+def appDir = "./" + configs.appNames.appName[0].'@newName'
 new File(appDir).mkdirs()
 def templatePath = "./template"
 createApp(templatePath, appDir, configs)
